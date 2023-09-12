a97bbdf77688d3521729290190cf5d600817a24f
ludvigs4 <144446967+ludvigs4@users.noreply.github.com>
2023-09-12 11:07:17 +0300

    Add files via upload

---
+++ Tests_Ludvigs_Marks.py
@@ -0,0 +1,12 @@
+#30 dienas python programmēšanas valodā
+name = "Marks"
+second_name = "Sidrevics"
+name_second_name = name + second_name
+country = "Sapain"
+city = "Bangladesh"
+age = 24
+year = 1940
+a, b, c = 10, "Mexico", "Flamingo"
+
+print(name, "\n", second_name, "\n", name_second_name, "\n", country, "\n", city, "\n", age, "\n", year, "\n", a, "\n", b, "\n", c)
+print(type(name), type(second_name), type(name_second_name), type(country), type(city), type(age), type(year), type(a), type(b), type(c))
