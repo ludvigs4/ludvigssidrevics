5fbe6e80c07d8f3d641cdecfda5e7cf7d17f9387
ludvigs4 <144446967+ludvigs4@users.noreply.github.com>
2023-11-03 12:07:00 +0200

    Add files via upload

--- aboli_sidrevics.py
+++ aboli_sidrevics.py
@@ -12,22 +12,22 @@ citrons_daudzums = int(input("Cik citronus tev vajag?"))
 kanelis_daudzums = float(input("Cik g kanēļa vajag?"))
 
 
-zaptes_daudzums = float(input("Cik jūs vārīsiet ievārījumu?"))
-
+abols_raza = float(input("Cik tev kg ābolu?"))
+print(f"Jūs varat izvārīt {round(abols_raza/abols_daudzums, 1)}l ievārījuma!")
 
 cukurs_majas = float(input("Cik tev kg cukura ir mājās?"))
 citrons_majas = float(input("Cik tev citroni mājās?"))
 kanelis_majas = float(input("Cik tev g kanēļa ir mājās?"))
 
+cukurs_cena = float(input("Cik maksā cukurs(1 kg)?"))
+citrons_cena = float(input("Cik maksā citrons(1 gab.)?"))
+kanelis_cena = float(input("Cik maksā kanēlis(15g)?"))
+
+litri = round(abols_raza / abols_daudzums, 1)
 
 cukurs_summa = math.ceil(cukurs_daudzums - cukurs_majas) * cukurs_cena
 citrons_summa = math.ceil(citrons_daudzums - citrons_majas) * citrons_cena
-kanelis_summa = -kanelis_majas
-
-if kanelis_daudzums % 15 <= 15:
-    kanelis_summa = math.floor(kanelis_daudzums / 15) * kanelis_cena
-else:
-    kanelis_summa = math.ceil(kanelis_daudzums / 15) * kanelis_cena
+kanelis_summa = kanelis_cena * 
 
 print("Cukurs maksā", cukurs_summa, "EUR")
 print("Citrons maksā", citrons_summa, "EUR")
