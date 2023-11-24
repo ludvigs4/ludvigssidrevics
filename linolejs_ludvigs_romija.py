92899a96cba9d986946c6cb683a4057e8477583a
ludvigs4 <144446967+ludvigs4@users.noreply.github.com>
2023-11-24 12:09:28 +0200

    Add files via upload

---
+++ linolejs_ludvigs_romija.py
@@ -0,0 +1,11 @@
+import math
+
+cena = float(input('Ievadi cenu uz kvadrātmetru: '))
+platums = float(input('Ievadi ruļļa platumu metros: '))
+garums = float(input('Ievadi ruļļa garumu: '))
+istabas_garums = float(input('Ievadi telpas garumu: '))
+istabas_platums = float(input('Ievadi telpas platumu: '))
+
+# rullu_daudzums = math.ceil(platiba/(platums*garums))
+# linolejs_daudzums = platiba
+# linoleja_cena = rullu_daudzums*cena
